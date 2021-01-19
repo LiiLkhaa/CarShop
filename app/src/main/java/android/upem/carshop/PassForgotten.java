7d1ea4693df36bfedde3a343ba585337a9dc6283
JANATI-IDRISSI-Imad <imad.janati.idrissi@gmail.com>
2021-01-19 14:17:22 +0100

    a

---
+++ app/src/main/java/android/upem/carshop/PassForgotten.java
@@ -0,0 +1,14 @@
+package android.upem.carshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassForgotten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_forgotten);
+    }
+}
