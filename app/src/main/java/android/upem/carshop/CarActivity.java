590011d0151ccc6f929f762d5a2e8de54fa95651
Saad Zridi <saadzridi25@gmail.com>
2021-01-25 01:16:50 +0100

    ButtonBack Beginning

--- app/src/main/java/android/upem/carshop/CarActivity.java
+++ app/src/main/java/android/upem/carshop/CarActivity.java
@@ -6,20 +6,27 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.upem.carshop.Fragement.CarFragment;
+import android.upem.carshop.Fragement.PanierFragment;
 import android.upem.carshop.models.Car;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.squareup.picasso.Picasso;
 
 
-public class CarActivity extends AppCompatActivity   {
+public class CarActivity extends AppCompatActivity {
     private Context context;
     public Car car;
 
@@ -35,6 +42,7 @@ public class CarActivity extends AppCompatActivity   {
         super.onCreate(savedInstanceState);
        // new GetCar().execute();
         setContentView(R.layout.activity_car);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         model = findViewById(R.id.model);
         name = findViewById(R.id.name);
@@ -42,14 +50,38 @@ public class CarActivity extends AppCompatActivity   {
         description = findViewById(R.id.description);
         imageView = findViewById(R.id.carimg);
 
-        name.setText(getIntent().getStringExtra("nameCar"));
-        model.setText(getIntent().getStringExtra("modelCar"));
+          name.setText(getIntent().getStringExtra("nameCar"));
+        //here i added this one to show the name of the car we want to show its details as a name of the activity
+          model.setText(getIntent().getStringExtra("modelCar"));
+          this.setTitle( name.getText().toString() + " | "+ model.getText().toString());
+
         price.setText(getIntent().getStringExtra("priceCar"));
         description.setText(getIntent().getStringExtra("descriptionCar"));
        Picasso.with(getBaseContext()).load(getIntent().getStringExtra("imageCar")).into(imageView);
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        int id = item.getItemId();
 
+        switch (id) {
+            case R.id.home:
+                Fragment registerDonor = new CarFragment();
+                fragmentTransaction.replace(R.id.fragment_container, registerDonor);
+                fragmentTransaction.commit();
+
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
 
 }
