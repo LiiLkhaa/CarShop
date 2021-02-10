62a1891e743e276ed0323471c20d6e28f2ea3e1e
driss21 <drissnfifi41@gmail.com>
2021-02-10 22:55:14 +0100

    HomeScreen (currency) most done

--- app/src/main/java/android/upem/carshop/HomeScreen.java
+++ app/src/main/java/android/upem/carshop/HomeScreen.java
@@ -13,21 +13,19 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.upem.carshop.Adapters.ImageAdapter;
 import android.upem.carshop.Adapters.PanierAdapter;
 import android.upem.carshop.Fragement.AccountActivityFragment;
 import android.upem.carshop.Fragement.CarFragment;
 import android.upem.carshop.Fragement.PanierFragment;
+import android.upem.carshop.handler.HttpHandler;
 import android.upem.carshop.models.User;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.internal.service.Common;
 import com.google.android.material.navigation.NavigationView;
 import com.nex3z.notificationbadge.NotificationBadge;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
@@ -43,6 +41,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,21 +50,26 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    private String devise;
+    private double rate;
+    private double solde = 15.0;
+    private TextView textView_solde_achat;
+    //Inofs User
     TextView nameUser, emailUser;
-    String email_user=null, name_user;
+    String email_user, name_user;
     User myUser;
     SliderView sliderView;
     CardView slidercard;
     NotificationBadge badge;
-    static NotificationBadge[] badges=new NotificationBadge[1];
+
     PanierAdapter panierAdapter;
-    Fragment carFragment;
-    PanierFragment panierFragmnt;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -77,17 +81,23 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
 
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
-        if(email_user==null){
-            email_user = getIntent().getStringExtra("Email");
-            Log.e("else","######### " +email_user);
-        }
+
         View headerView = navigationView.getHeaderView(0);
         emailUser =(TextView) headerView.findViewById(R.id.emailHeaderNV);
         nameUser = (TextView) headerView.findViewById(R.id.fullNameHeaderNv);
-        // hadi hiya li khasha tkon
-        new getUser().execute();
+        email_user = getIntent().getStringExtra("Email");// hadi hiya li khasha tkon
 
-         new getUser().execute();
+        //email_user=emailUser.getText().toString();//hadi ghi mo2aqatan 7it makandiroch connection
+
+
+        new getUser().execute();
+        Log.e("email_user","######### " +email_user);
+
+        // email_user=emailUser.getText().toString();//hadi ghi mo2aqatan 7it makandiroch connection
+        // Log.e("email_user","######### " +email_user);
+
+        new getUser().execute();
+        // emailUser.setText(myUser.getEmail());
 
         slidercard = findViewById(R.id.slidercard);
         //slider
@@ -99,25 +109,24 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
         images.add(R.drawable.rang);
         images.add(R.drawable.tesla);
         ImageAdapter imageAdapter = new ImageAdapter(images);
+
         sliderView.setSliderAdapter(imageAdapter);
         sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
         sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
         sliderView.startAutoCycle();
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.e("onStart","################################ " +"ana t7alit");
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         getMenuInflater().inflate(R.menu.menu_action_bar, menu);
         View view = menu.findItem(R.id.cart_panier).getActionView();
         badge = view.findViewById(R.id.badge_cart);
         //badge.setText("3");
-        badges[0]=badge;
+
+        updateCartCount();
         return true;
     }
 
@@ -126,8 +135,13 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                new GetSizeCarInCart().execute(email_user);
-                Log.println(Log.INFO,"GetSizeCarInCart","1");
+                //if(panierAdapter.getItemCount() == 0){
+                // badge.setVisibility(View.INVISIBLE);
+                // }
+                //else {
+                // badge.setVisibility(View.VISIBLE);
+                //badge.setText();
+                // }
             }
         });
     }
@@ -135,8 +149,7 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
     @Override
     protected void onResume() {
         super.onResume();
-        new GetSizeCarInCart().execute(email_user);
-        Log.println(Log.INFO,"onResume","1");
+        updateCartCount();
     }
 
     @Override
@@ -152,6 +165,12 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
                 drawerLayout.closeDrawers();
                 slidercard.setVisibility(View.INVISIBLE);
                 break;
+            case R.id.dollar:
+                devise = "USD";
+                new ChangeCurrencyTask().execute(devise);
+            case R.id.mad:
+                devise = "MAD";
+                new ChangeCurrencyTask().execute(devise);
         }
         return true;
     }
@@ -159,7 +178,6 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
     @Override
     public void onBackPressed() {
         if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-
             drawerLayout.closeDrawer(GravityCompat.START);
         }else{
             super.onBackPressed();
@@ -174,14 +192,14 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
 
         switch (id) {
             case R.id.car:
-                carFragment = CarFragment.newInstance(email_user,drawerLayout,fragmentTransaction);
-                fragmentTransaction.replace(R.id.fragment_container, carFragment);
+                Fragment registerDonor = CarFragment.newInstance(email_user,drawerLayout,fragmentTransaction);
+                fragmentTransaction.replace(R.id.fragment_container, registerDonor);
                 fragmentTransaction.commit();
                 drawerLayout.closeDrawers();
                 slidercard.setVisibility(View.INVISIBLE);
                 break;
             case R.id.panier:
-                panierFragmnt =  PanierFragment.newInstance(email_user);
+                Fragment panierFragmnt =  PanierFragment.newInstance(email_user);
                 fragmentTransaction.replace(R.id.fragment_container, panierFragmnt);
                 fragmentTransaction.commit();
                 drawerLayout.closeDrawers();
@@ -211,10 +229,44 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
                 break;
 
 
-    }
+        }
         return true;
-}
+    }
 
+    private class ChangeCurrencyTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(String... arg0) {
+            String url = "https://carsho.herokuapp.com/api/currency/"+arg0[0];
+            HttpHandler sh = new HttpHandler();
+            String result = sh.makeServiceCall(url);
+            rate = Double.parseDouble(result);
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            Log.e("@Devise","Solde-2>"+devise);
+            Log.e("@Rate","Solde-2>"+rate);
+
+        }
+    }
+
+    public String getPriceProduct(Double price){
+        Double prix = price*rate;
+        DecimalFormat df = new DecimalFormat("0.00");
+        String result  = df.format(prix)+" " +devise;
+        return result ;
+    }
     public class getUser extends AsyncTask<Void, Void, String> {
         HttpURLConnection urlConnection;
         @Override
@@ -249,7 +301,7 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
             try {
 
                 JSONObject userJSON=new JSONObject(s);
-                 myUser = User.UserParserJSON(userJSON);
+                myUser = User.UserParserJSON(userJSON);
                 //Toast.makeText(getBaseContext(), "User Name"+ myUser.getName(), Toast.LENGTH_LONG).show();
                 Log.e("Name", "email user : "+ myUser.getName());
                 //test
@@ -263,60 +315,6 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
         }
 
     }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        outState.putString("email",email_user);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e("onDestroy","################################ " +"ana tsadit");
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        email_user=savedInstanceState.getString("email");
-        Log.e("onRestoreInstanceState","################################ " +savedInstanceState.getString("email"));
-    }
-
-
-
-    public static class GetSizeCarInCart extends AsyncTask<String, Void, String> {
-        HttpURLConnection urlConnection;
-        @Override
-        protected String doInBackground(String... email) {
-            StringBuilder result = new StringBuilder();
-            try {
-                String u="https://carsho.herokuapp.com/Cart/getSizeCarInCart/"+email[0];
-                URL url = new URL(u);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                }
-            }catch( Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                urlConnection.disconnect();
-            }
-            return result.toString();
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            badges[0].setText(s);
-        }
-    }
-
 
 }
 
