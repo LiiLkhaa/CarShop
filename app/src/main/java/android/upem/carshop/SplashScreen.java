13ccc364253c65ec598bdc02ea5efe84311606c7
LiiLkhaa <khalilbakri@gmail.com>
2021-02-10 13:43:23 +0100

    Splash Screen to load Server

--- app/src/main/java/android/upem/carshop/SplashScreen.java
+++ app/src/main/java/android/upem/carshop/SplashScreen.java
@@ -3,8 +3,17 @@ package android.upem.carshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -14,13 +23,40 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         getSupportActionBar().hide();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        new LoadServer().execute();
+    }
+
+    public class LoadServer extends AsyncTask<Long, Void, String> {
+        HttpURLConnection urlConnection;
+        @Override
+        protected String doInBackground(Long... ids) {
+            StringBuilder result = new StringBuilder();
+            try {
+                String u="https://carsho.herokuapp.com/";
+                URL url = new URL(u);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+            }catch( Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                urlConnection.disconnect();
+            }
+
+            return result.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
                 Intent i = new Intent(SplashScreen.this,Login.class);
                 startActivity(i);
-                finish();
             }
-        }, 3000);
     }
 }
